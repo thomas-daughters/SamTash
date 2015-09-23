7899c5b1dd460c805d4872ced810081b0f54a571
thoma87 <thomas.sam@me.com>
2015-09-23 22:22:40 +0100

    Initial

---
+++ Wedding/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Mail;
+using System.Web.Mvc;
+using Wedding.Models;
+
+namespace Wedding.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			return View();
+		}
+
+		public ActionResult Location() {
+			return View();
+		}
+
+		public ActionResult RSVP() {
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Thanks(RSVP rsvp) {
+			string body = "Names: " + rsvp.Names + Environment.NewLine
+				+ "Email: " + rsvp.Email + Environment.NewLine
+				+ "Available: " + rsvp.Available.ToString() + Environment.NewLine
+				+ "Comments: " + rsvp.Comments;
+
+			MailMessage message = new MailMessage("wedding@samtash.com", "wedding@samtash.com", "Wedding RSVP", body);
+			SmtpClient client = new SmtpClient("relay-hosting.secureserver.net", 25);
+			client.Send(message);
+
+			ViewBag.Message = rsvp.Available ? "Great! We look forward to seeing you there." : "Sorry you can't join us.";
+
+			return View();
+		}
+	}
+}
---
+++ Wedding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wedding.MvcApplication" Language="C#" %>
