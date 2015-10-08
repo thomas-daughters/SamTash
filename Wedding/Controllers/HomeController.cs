600f1092f7326bfab9220b390d87cd73ac624125
thoma87 <thomas.sam@me.com>
2015-10-08 10:20:08 +0100

    Homepage has static heart on venue Acccommodation page complete RSVP page complete Photos page complete, with photos Various cosmetic improvements

--- Wedding/Controllers/HomeController.cs
+++ Wedding/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using Wedding.Models;
 namespace Wedding.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Index() {
-			return View();
+			return View("Home");
 		}
 
 		public ActionResult Location() {
@@ -22,21 +22,18 @@ namespace Wedding.Controllers {
 		}
 
 		public ActionResult Photos() {
-			return View("Index");
+			return View();
 		}
 
 		[HttpPost]
 		public ActionResult Thanks(RSVP rsvp) {
-			string body = "Names: " + rsvp.Names + Environment.NewLine
-				+ "Email: " + rsvp.Email + Environment.NewLine
-				+ "Available: " + rsvp.Available.ToString() + Environment.NewLine
-				+ "Comments: " + rsvp.Comments;
+			string body = "Names: " + rsvp.Names;
 
 			MailMessage message = new MailMessage("wedding@samtash.com", "wedding@samtash.com", "Wedding RSVP", body);
 			SmtpClient client = new SmtpClient("relay-hosting.secureserver.net", 25);
 			client.Send(message);
 
-			ViewBag.Message = rsvp.Available ? "Great! We look forward to seeing you there." : "Sorry you can't join us.";
+			ViewBag.Message = "Great! We look forward to seeing you there.";
 
 			return View();
 		}
--- Wedding/Models/RSVP.cs
+++ Wedding/Models/RSVP.cs
@@ -3,17 +3,6 @@
 namespace Wedding.Models {
 	public class RSVP {
 		[Required]
-		[Display(Name = "Your Name(s)", Prompt = "Guest + Partner + Children")]
 		public string Names { get; set; }
-
-		[Required]
-		[EmailAddress]
-		public string Email { get; set; }
-
-		[Required]
-		[Display(Name = "Are You Available?")]
-		public bool Available { get; set; }
-
-		public string Comments { get; set; }
 	}
 }
